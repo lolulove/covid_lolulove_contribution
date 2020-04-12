39089451bd46921cd0f52a1ea4ef45ca124ba24e
lolulove <32493664+lolulove@users.noreply.github.com>
2020-04-12 17:08:21 +0100

    Challenge 1 completed

--- src/estimator.py
+++ src/estimator.py
@@ -1,2 +1,46 @@
+def infectionsCalcution(periodType, timeToElapse):
+    if periodType == 'months':
+        normalizedDays = 30 * timeToElapse
+
+    elif periodType == 'weeks':
+        normalizedDays = 7 * timeToElapse
+
+    else:
+        normalizedDays = timeToElapse
+
+    infectionFactor = normalizedDays // 3
+
+    return 2 ** infectionFactor
+
+
+def impact_cal(data={}):
+    impact = {
+        'currentlyInfected': data['reportedCases'] * 10
+    }
+
+    impact = {
+        'currentlyInfected': data['reportedCases'] * 10,
+        'infectionsByRequestedTime': impact['currentlyInfected'] * infectionsCalcution(data['periodType'],
+                                                                                       data['timeToElapse'])}
+
+    return impact
+
+
+def severeImpact_cal(data={}):
+    severeImpact = {
+        'currentlyInfected': data['reportedCases'] * 50
+    }
+
+    severeImpact = {
+        'currentlyInfected': data['reportedCases'] * 50,
+        'infectionsByRequestedTime': severeImpact['currentlyInfected'] * infectionsCalcution(data['periodType'],
+                                                                                             data['timeToElapse'])}
+    return severeImpact
+
+
 def estimator(data):
-  return data
+    return {
+        'data': data,
+        'impact': impact_cal(data),
+        'severeImpact': severeImpact_cal(data)
+    }
