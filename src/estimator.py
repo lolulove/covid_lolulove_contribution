d5bf33bcd7bfae8ada3f8160ec6c6e7e5bc4a61b
lolulove <32493664+lolulove@users.noreply.github.com>
2020-04-14 22:10:52 +0100

    Challenge 2 completed

--- src/estimator.py
+++ src/estimator.py
@@ -36,7 +36,7 @@ def dayCalcution(periodType, timeToElapse):
     return normalizedDays
 
 
-def impact_cal(data={}):
+def impact_cal(data):
     impact = {
         'currentlyInfected': data['reportedCases'] * 10
     }
@@ -68,7 +68,7 @@ def impact_cal(data={}):
     return impact
 
 
-def severeImpact_cal(data={}):
+def severeImpact_cal(data):
     severeImpact = {
         'currentlyInfected': data['reportedCases'] * 50
     }
